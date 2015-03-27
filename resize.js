86503805a3140ba3e23bc4f6d138a0b91da34691
Seungho Yang <smattyang@gmail.com>
2015-03-26 20:28:26 -0700

    Made boolean operations more concise

--- resize.js
+++ resize.js
@@ -2,7 +2,7 @@
 window.onload = function() {
 
 	// Styling
-	$('<style type="text/css">#resize {position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); font-family: Helvetica; font-size: 42px; opacity: 0; transition: all 0.2s cubic-bezier(0.4, 0.0, 0.2, 1);}</style>').appendTo($('head'));
+	$('<style type="text/css">#resize {position: absolute; top: 50%; left: 50%; -webkit-transform: translate(-50%, -50%); transform: translate(-50%, -50%); font-family: Helvetica; font-size: 42px; opacity: 0; transition: all 0.2s cubic-bezier(0.4, 0.0, 0.2, 1);}</style>').appendTo($('head'));
 	
 	// Resize div
 	$("body").append('<div id="resize"><span id="width"></span> <span id="times">&times;</span> <span id="height"></span></span></div>')
@@ -39,27 +39,14 @@ $(window).resize(function() {
 
 
 	// Boolean
-	if (ww == desiredWidth) {
-		$("#width").css('color', green);
-	}
+	var color;
 
-	if (ww != desiredWidth) {
-		$("#width").css('color', grey);
-	}
+	color = (ww == desiredWidth ? green : grey);
+	$("#width").css('color', color);
 
-	if (wh == desiredHeight) {
-		$("#height").css('color', green);
-	}
+	color = (wh == desiredHeight ? green : grey);
+	$("#height").css('color', color);
 
-	if (wh != desiredHeight) {
-		$("#height").css('color', grey);
-	}
-
-	if (ww == desiredWidth && wh == desiredHeight) {
-		$("#times").css('color', green);
-	}
-
-	if (ww != desiredWidth || wh != desiredHeight) {
-		$("#times").css('color', grey);
-	}
+	color = (ww == desiredWidth && wh == desiredHeight ? green : grey);
+	$("#times").css('color', color);
 });

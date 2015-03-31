640f70ebb297e6add763ef3619bc342034b3560a
seunghoyang <seunghoyang@seunghoyang-macbookpro.roam.corp.google.com>
2015-03-30 19:01:01 -0700

    Added backslashes to make appends more legible

--- resize.js
+++ resize.js
@@ -2,10 +2,25 @@
 window.onload = function() {
 
 	// Styling
-	$('<style type="text/css">#resize {position: absolute; top: 50%; left: 50%; -webkit-transform: translate(-50%, -50%); transform: translate(-50%, -50%); font-family: Helvetica; font-size: 42px; opacity: 0; transition: all 0.2s cubic-bezier(0.4, 0.0, 0.2, 1);}</style>').appendTo($('head'));
+	$('<style type="text/css"> \
+		#resize { \
+			position: absolute; \
+			top: 50%; left: 50%; \
+			-webkit-transform: translate(-50%, -50%); \
+			transform: translate(-50%, -50%); \
+			font-family: Helvetica; \
+			font-size: 42px; \
+			opacity: 0; \
+			transition: all 0.2s cubic-bezier(0.4, 0.0, 0.2, 1); \
+		} \
+		</style>').appendTo($('head'));
 	
 	// Resize div
-	$("body").append('<div id="resize"><span id="width"></span> <span id="times">&times;</span> <span id="height"></span></span></div>')
+	$("body").append('<div id="resize"> \
+		<span id="width"></span> \
+		<span id="times">&times;</span> \
+		<span id="height"></span> \
+		</div>')
 };
 
 // Desired window dimensions
